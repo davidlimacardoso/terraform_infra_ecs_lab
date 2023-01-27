ef9466a374a4cd07e39e6e8b706560945bfecde0
davidlimacardoso <david.lima.cd@gmail.com>
2023-01-26 22:35:54 -0300

    Set target group ALB

--- infra/alb.tf
+++ infra/alb.tf
@@ -3,3 +3,26 @@ resource "aws_lb" "alb" {
   security_groups    = [aws_security_group.alb.id]
   subnets            = [module.vpc.public_subnets]
 }
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = "8000"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.target.arn
+  }
+}
+
+resource "aws_lb_target_group" "target" {
+  name        = "ecs_django"
+  port        = 8000
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = module.vpc.vpc_id
+}
+
+output "ip" {
+    value = aws_lb.alb.dns_name
+}
